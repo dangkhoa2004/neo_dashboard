cff8a0be050ca8af5c9dc6e698c630e63e290e9a
Cao Đăng Khoa <149858139+dangkhoa2004@users.noreply.github.com>
2025-12-26 09:16:33 +0700

    [UPDATE] Config Host

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // QUAN TRỌNG: Cho phép truy cập từ IP công khai
+    port: 5173,      // Cố định cổng 5173
+  }
 })
